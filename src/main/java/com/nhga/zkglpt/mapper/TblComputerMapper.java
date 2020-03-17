8ef4d4c792548070f8ea8342cc476f681d852ecc
admin <51804474@qq.com>
2020-03-17 16:38:18 +0800

    second

--- src/main/java/com/nhga/zkglpt/mapper/TblComputerMapper.java
+++ src/main/java/com/nhga/zkglpt/mapper/TblComputerMapper.java
@@ -2,12 +2,6 @@ package com.nhga.zkglpt.mapper;
 
 import com.diboot.core.mapper.BaseCrudMapper;
 import com.nhga.zkglpt.model.TblComputer;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.One;
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.Results;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.mapping.FetchType;
 
 /**
  * <p>
